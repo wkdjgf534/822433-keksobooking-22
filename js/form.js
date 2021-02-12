4587596090475056946e4ec9c9fad8edbdff6f27
Eugene Makarihkin <eugene@makarikhin.info>
2021-02-13 00:04:57 +0500

    Added constants

--- js/form.js
+++ js/form.js
@@ -1,1 +1,16 @@
 // Form
+
+//const MIN_TITLE_LENGTH = 30;
+//const MAX_TITLE_LENGTH = 100;
+
+//const MIN_PRICE = 0;
+//const MAX_PRICE = 1000000;
+
+//const TYPES = ['bungalo', 'flat', 'house', 'palace'];
+//const MIN_PRICES = ['0', '1000', '5000', '10000'];
+//const GUEST_ROOMS = {
+//  1: ['1'],
+//  2: ['2', '1'],
+//  3: ['3', '2', '1'],
+//  100: ['0'],
+//};
