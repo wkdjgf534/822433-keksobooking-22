876cf3fecf8419797b3a902493c78e6d8026b6a3
Eugene Makarihkin <eugene@makarikhin.info>
2021-02-15 01:10:47 +0500

    Added new functions to check offer's parmas

--- js/card.js
+++ js/card.js
@@ -16,25 +16,52 @@ const  appartmentTypes = {
 
 const generateFeaturePictograms = (card, offer) => {
   const features = card.querySelector('.popup__features');
-  features.innerHTML = '';
 
-  offer.features.forEach(element => {
-    const featureClasses = ['popup__feature', `popup__feature--${element}`]
-    const feature = makeElement(featureClasses, 'li');
-    features.appendChild(feature);
-  });
+  if (offer.features) {
+    features.innerHTML = '';
+    offer.features.forEach(element => {
+      const featureClasses = ['popup__feature', `popup__feature--${element}`]
+      const feature = makeElement(featureClasses, 'li');
+      features.appendChild(feature);
+    });
+  }
 };
 
 const generatePhotoThumbnails = (card, offer) => {
   const photos = card.querySelector('.popup__photos');
   const blankPhoto = photos.querySelector('.popup__photo');
-  photos.removeChild(blankPhoto); // Удаляем текстовое поле
+  photos.removeChild(blankPhoto);
 
-  offer.photos.forEach(path => {
-    const photoClasses = ['popup__photo'];
-    const photo = makeImage(photoClasses, path, THUMBNAIL_WIDTH, THUMBNAIL_HEIGHT);
-    photos.appendChild(photo);
-  });
+  if (offer.photos) {
+    offer.photos.forEach(path => {
+      const photoClasses = ['popup__photo'];
+      const photo = makeImage(photoClasses, path, THUMBNAIL_WIDTH, THUMBNAIL_HEIGHT);
+      photos.appendChild(photo);
+    });
+  }
+}
+
+const generateScheduleTime = (card, offer) => {
+  const text = `Заезд после ${offer.checkin} выезд до ${offer.checkout}`;
+  const shcheduleTimeOffer = card.querySelector('.popup__text--time');
+  (offer.checkin && offer.checkout) ? shcheduleTimeOffer.textContent = text : shcheduleTimeOffer.remove();
+};
+
+const generateAvatar = (card, author) => {
+  const userAvatar = card.querySelector('.popup__avatar');
+  author.avatar ? userAvatar.src = author.avatar : userAvatar.remove();
+};
+
+const generateCapacity = (card, offer) => {
+  const text = `${offer.rooms} комнаты для ${offer.guests} гостей`;
+  const capacityOffer = card.querySelector('.popup__text--capacity');
+  (offer.rooms && offer.guests) ? capacityOffer.textContent = text : capacityOffer.remove();
+};
+
+const generatePrice = (card, offer) => {
+  const text = `${offer.price} ₽/ночь`;
+  const priceOffer = card.querySelector('.popup__text--price');
+  offer.price ? priceOffer.textContent = text : priceOffer.remove();
 }
 
 const cards = [];
@@ -45,14 +72,15 @@ const createCard = (data) => {
   const author = data.author;
   card.querySelector('.popup__title').textContent = offer.title;
   card.querySelector('.popup__text--address').textContent = offer.address;
-  card.querySelector('.popup__text--price').textContent = `${offer.price} ₽/ночь`;
   card.querySelector('.popup__type').textContent = appartmentTypes[offer.type];
-  card.querySelector('.popup__text--capacity').textContent = `${offer.rooms} комнаты для ${offer.guests} гостей`;
-  card.querySelector('.popup__text--time').textContent = `Заезд после ${offer.checkin} выезд до ${offer.checkout}`;
   card.querySelector('.popup__description').textContent = offer.description;
-  card.querySelector('.popup__avatar').src = author.avatar;
+  generatePrice(card, offer);
+  generateCapacity(card, offer);
+  generateScheduleTime(card, offer);
   generateFeaturePictograms(card, offer);
   generatePhotoThumbnails(card, offer);
+  generateAvatar(card, author);
+
   return card;
 };
 
