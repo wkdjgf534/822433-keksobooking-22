c9aae908c8ac128cd99c1af32655c6e113d85572
Eugene Makarihkin <eugene@makarikhin.info>
2021-02-20 21:01:27 +0500

    Fixed grammar mistakes

--- js/utils.js
+++ js/utils.js
@@ -1,7 +1,7 @@
 // Utils
 
 const findOne = (selector, el = document) => el.querySelector(selector)
-// querySelectorAll возвращает NodeList, оператор spread преобразкет его в масив
+// querySelectorAll возвращает NodeList, оператор spread преобразует его в массив
 const findAll = (selector, el = document) => [...el.querySelectorAll(selector)]
 
 export {findOne, findAll}
