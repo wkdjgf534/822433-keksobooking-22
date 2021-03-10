8288617807b7ac14a617ad88518eae3296fd6dfa
Eugene Makarihkin <eugene@makarikhin.info>
2021-03-10 23:39:17 +0500

    Added webpack config

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+
+const path = require('path')
+
+module.exports = {
+  entry: './source/js/main.js',
+  devtool: 'source-map',
+  output: {
+    filename: 'main.bundle.js',
+    path: path.resolve(__dirname, 'build/js'),
+  }
+}
